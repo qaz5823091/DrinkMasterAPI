89d3302872f9125fb79180d6663e8d76a72ba7ac
qaz5823091 <r19991106@gmail.com>
2023-07-13 19:55:33 +0800

    only get and post modification

--- api/server.js
+++ api/server.js
@@ -5,10 +5,18 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+
+server.use((req, res, next) => {
+    if (req.method === 'POST') {
+        req.method = 'GET'
+        req.query = req.body
+    }
+    next()
+})
+
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+    '/api/*': '/$1'
 }))
 server.use(router)
 server.listen(3000, () => {
